816f567f86be61004f2f2bd0d7cfa85bf98d1862
Trim21 <trim21.me@gmail.com>
2025-09-08 22:38:43 +0800

    fix: skip int struct field (#71)

--- internal/decoder/context.go
+++ internal/decoder/context.go
@@ -80,6 +80,31 @@ func skipClassName(buf []byte, cursor int64) (int64, error) {
 	return end - 2, err
 }
 
+// i:{};
+// the cursor should point to the beginning `i`
+// and it will point to the byte after `;`
+func skipInt(buf []byte, cursor int64) (int64, error) {
+	cursor++
+
+	if buf[cursor] != ':' {
+		return cursor, errors.ErrUnexpected("':' after type operator `i`", cursor, buf[cursor])
+	}
+
+	cursor++
+
+	for isInteger[buf[cursor]] {
+		cursor++
+	}
+
+	if buf[cursor] != ';' {
+		return cursor, errors.ErrUnexpected("';' after integer", cursor, buf[cursor])
+	}
+
+	cursor++
+
+	return cursor, nil
+}
+
 func skipString(buf []byte, cursor int64) (int64, error) {
 	cursor++
 	sLen, end, err := readLength(buf, cursor)
@@ -141,12 +166,7 @@ func skipValue(buf []byte, cursor, depth int64) (int64, error) {
 		return skipString(buf, cursor)
 	// case 'd':
 	case 'i':
-		cursor++
-		end, err := skipLengthWithBothColon(buf, cursor)
-		if err != nil {
-			return cursor, err
-		}
-		return end + 1, nil
+		return skipInt(buf, cursor)
 	case 'b':
 		cursor++
 		if buf[cursor] != ':' {
